53c3732cd0901e2b19691d1c0d9bb79079e49fc8
Jordan Yeung <yeungjordan@hotmail.com>
2024-08-29 19:29:21 -0400

    created home page controller

---
+++ controllers/home-controller.js
@@ -0,0 +1,39 @@
+const knex = require("../knexfile");
+
+const getTeams = async (req, res) => {
+  try {
+    const teams = await knex("teams").select("*");
+    res.json({ teams });
+  } catch (error) {
+    res.status(500).json({ message: " Error fetching teams", error });
+  }
+};
+
+const createGame = async (req, res) => {
+  const { homeTeam, awayTeam, runsFor, runsAgainst, date } = req.body;
+
+  try {
+    const gameId = await knew("games")
+      .insert({
+        date,
+        home_team_id: homeTeam,
+        away_team_id: awayTeam,
+        runs_for: runsFor,
+        runs_against: runsAgainst,
+        created_at: knex.fn.now(),
+        updated_at: knex.fn.now(),
+      })
+      .returning("id");
+
+    await knex("team_games").insert([
+      { team_id: homeTeam, game_id: gameId[0] },
+      { team_id: awayTeam, game_id: gameId[0] },
+    ]);
+
+    res.status(201).json({ message: "Game created successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Error creating game", error });
+  }
+};
+
+module.exports = { getTeams, createGame };

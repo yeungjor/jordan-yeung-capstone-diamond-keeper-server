d2f7051d00686581511050659f8d16b6240f3d98
Jordan Yeung <yeungjordan@hotmail.com>
2024-08-29 20:26:20 -0400

    create players controller

---
+++ controllers/players-controller.js
@@ -0,0 +1,34 @@
+const knex = require("../knexfile");
+
+// Fetch all players for specific team
+const getTeamPlayers = async (req, res) => {
+  const { team_id } = req.params;
+
+  try {
+    const players = await knex("players").where({ team_id }).select("*");
+    res.json({ players });
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching players", error });
+  }
+};
+
+const createPlayer = async (req, res) => {
+  const { player_name, jersey_number, gender, bats, throws, team_id } =
+    req.body;
+
+  try {
+    await knex("players").insert({
+      player_name,
+      jersey_number,
+      gender,
+      bats,
+      throws,
+      team_id,
+    });
+    res.status(201).json({ message: "Player created successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Error creating player", error });
+  }
+};
+
+module.exports = { getTeamPlayers, createPlayer };
